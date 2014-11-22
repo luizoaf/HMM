0c997b7d610240273ce4717e6238142f22511ec4
Luiz <luizoaf@gmail.com>
2014-11-22 20:06:53 -0300

    forward funcionando, necessita refatorar o código

--- HMM.R
+++ HMM.R
@@ -32,13 +32,25 @@ pi = matrix(c(.6,.4,0),nrow=3,ncol=1)
 #inicializacao
 a1 = B[,1] * pi
 a1
-
+alphas = data.frame()
 i = 1
-sum((a1*retorna_a_pelo_indice_coluna(A,i))) * retorna_b_pelo_indice_linha(B,i)# OK 
-i=2
-sum((a1*retorna_a_pelo_indice_coluna(A,i))) * retorna_b_pelo_indice_linha(B,i)# OK 
-if(i )
-  i=3
-sum((a1*retorna_a_pelo_indice_coluna(A,i))) * retorna_b_pelo_indice_linha(B,i)# OK 
+a2_1 = sum((a1*retorna_a_pelo_indice_coluna(A,i))) * retorna_b_pelo_indice_linha(B,i)# OK 
+i = 2
+a2_2 = sum((a1*retorna_a_pelo_indice_coluna(A,i))) * retorna_b_pelo_indice_linha(B,i)# OK 
+alphas = rbind(data.frame(a2_1,a2_1,a2_2))
+colnames(alphas) = c("alpha1","alpha2","alpha3")
 
+aux_alphas =  data.frame(1)
+j = 1
+for(i in 3:8){
+  aux_alphas=  cbind(aux_alphas,sum(alphas[nrow(alphas),] *retorna_a_pelo_indice_coluna(A,i)) * retorna_b_pelo_indice_linha(B,i))# OK 
+  if(i%%3 ==0){
+    aux_alphas = data.frame(aux_alphas[,-1])
+  }
+  if(ncol(aux_alphas)==3){
+    alphas = rbind(data.frame( aux_alphas[,1], aux_alphas[,2], aux_alphas[,3]))
+    colnames(alphas) = c("alpha1","alpha2","alpha3")
+    aux_alphas =  data.frame(1)
+  }
+}
 
