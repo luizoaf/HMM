3df03273988d26aac2bc2aaa558c7454ef6f56cf
Luiz <luizoaf@gmail.com>
2014-11-23 12:15:43 -0300

    Viterbi, etapa 1 e 2 OK

--- HMM.R
+++ HMM.R
@@ -2,8 +2,8 @@
 # Disciplina: Probabilidade e Processos Estocásticos
 # Professor: Mêuser
 
-retorna_b_pelo_indice_linha = function(B,indice_b){
-  j_linha = 2
+retorna_b_pelo_indice_linha = function(B,indice_b,indice_linha_b){
+  j_linha = indice_linha_b
   j_coluna = 1
   i=1
   while( i <=indice_b){
@@ -36,6 +36,39 @@ retorna_a_pelo_indice_pela_linha = function(A,indice_a){
   }
 }
 
+hmm_forward = function(A,B,pi,iteracoes){
+  # Inicialização (Equação 6)
+  a1 = B[,1] * pi
+  alphas = data.frame()
+  a2_1 = sum((a1*retorna_a_pelo_indice_coluna(A,1))) * retorna_b_pelo_indice_linha(B,1,2)
+  a2_2 = sum((a1*retorna_a_pelo_indice_coluna(A,2))) * retorna_b_pelo_indice_linha(B,2,2) 
+  alphas = rbind(data.frame(a2_1,a2_1,a2_2))
+  colnames(alphas) = c("alpha1","alpha2","alpha3")
+  #   print(alphas)
+  
+  # Indução (Equação 7)
+  aux_alphas =  data.frame(1)
+  j = 1
+  for(i in ncol(A):iteracoes){
+    aux_alphas=  cbind(aux_alphas,sum(alphas[nrow(alphas),] *retorna_a_pelo_indice_coluna(A,i)) * retorna_b_pelo_indice_linha(B,i,2))# OK 
+    if(i%%ncol(A) ==0){
+      aux_alphas = data.frame(aux_alphas[,-1])
+    }
+    if(ncol(aux_alphas)==ncol(A)){
+      alphas = rbind(data.frame( aux_alphas[,1], aux_alphas[,2], aux_alphas[,3]))
+      #       print(alphas)
+      colnames(alphas) = c("alpha1","alpha2","alpha3")
+      aux_alphas =  data.frame(1)
+    }
+  }
+  
+  ################### 2. Cálculo de P(O | lambda ) ####################
+  # Terminação (Equação 8)
+  print(alphas)
+  return(sum(alphas))
+}
+
+
 hmm_backward = function(A,B,pi,iteracoes){
   partes_b = iteracoes/ncol(B)
   indice_coluna_b = ncol(B)
@@ -57,42 +90,16 @@ hmm_backward = function(A,B,pi,iteracoes){
       colnames(betas) = c("beta1","beta2","beta3")
     }
   }
-  print(betas[nrow(betas)-1,])
-  sum(pi*betas[nrow(betas)-1,]*B[,1])
-}
-
-
-hmm_forward = function(A,B,pi,iteracoes){
-  # Inicialização (Equação 6)
-  a1 = B[,1] * pi
-  alphas = data.frame()
-  a2_1 = sum((a1*retorna_a_pelo_indice_coluna(A,1))) * retorna_b_pelo_indice_linha(B,1)
-  a2_2 = sum((a1*retorna_a_pelo_indice_coluna(A,2))) * retorna_b_pelo_indice_linha(B,2) 
-  alphas = rbind(data.frame(a2_1,a2_1,a2_2))
-  colnames(alphas) = c("alpha1","alpha2","alpha3")
-  #   print(alphas)
-  
-  # Indução (Equação 7)
-  aux_alphas =  data.frame(1)
-  j = 1
-  for(i in ncol(A):iteracoes){
-    aux_alphas=  cbind(aux_alphas,sum(alphas[nrow(alphas),] *retorna_a_pelo_indice_coluna(A,i)) * retorna_b_pelo_indice_linha(B,i))# OK 
-    if(i%%ncol(A) ==0){
-      aux_alphas = data.frame(aux_alphas[,-1])
-    }
-    if(ncol(aux_alphas)==ncol(A)){
-      alphas = rbind(data.frame( aux_alphas[,1], aux_alphas[,2], aux_alphas[,3]))
-      #       print(alphas)
-      colnames(alphas) = c("alpha1","alpha2","alpha3")
-      aux_alphas =  data.frame(1)
+  for(i in 1:nrow(betas)){
+    if(betas$beta3[i]==0){
+      print(betas[i,])
+      return(sum(pi*betas[i,]*B[,1]))      
     }
   }
-  
-  ################### 2. Cálculo de P(O | lambda ) ####################
-  # Terminação (Equação 8)
-    print(alphas)
-  return(sum(alphas))
+  #   print(betas[nrow(betas)-1,])
+  #   sum(pi*betas[nrow(betas)-1,]*B[,1])
 }
+
 
 
 A = matrix(c(.3,0,0,.5,.3,0,.2,.7,1),nrow=3,ncol=3)
@@ -103,8 +110,28 @@ pi = matrix(c(.6,.4,0),nrow=3,ncol=1)
 hmm_forward(A,B,pi,8)
 hmm_backward(A,B,pi,12)
 
+# algoritmo de Viterbi
 
+# Inicialização (Equações 15a e 15b)
+delta = pi*B[,1]
+psi = rep(0,nrow(B))
 
+delta2_1 = max(delta*A[,1])*B[1,1]
+psi2_1 = which.max(delta*A[,1])
 
+delta2_2 = max(delta*A[,2])*B[2,1]
+psi2_2 = which.max(delta*A[,2])
+
+delta2_3 = max(delta*A[,3])*B[3,1]
+psi2_3 = which.max(delta*A[,3])
+
+iteracoes = 18
+
+for(i in 1:iteracoes){
+  a = retorna_a_pelo_indice_coluna(A,i-1)
+#   b = retorna_b_pelo_indice_linha(B,i,1)
+#   print(b)
+  print(a)
+}
 
 
